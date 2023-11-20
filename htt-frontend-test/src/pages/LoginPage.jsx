38fabb3b865609e92728c994c9eb748fbf854ebb
siriusoya <siriusoya@gmail.com>
2023-11-20 21:48:37 +0700

    created login page

---
+++ htt-frontend-test/src/pages/LoginPage.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+const LoginPage = () => {
+    const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    // await fetch endpoint login kemudian kirim data payload sebagai body
+    localStorage.setItem("access_token", "access token dari endpoint login");
+        navigate("/");
+  };
+
+  return (
+    <div className="login_container">
+      <div className="login_modal">
+        <h2 id="login_title">LOGIN</h2>
+        <hr id="login_underline" />
+        <form id="login_form" onSubmit={handleSubmit(onSubmit)}>
+          
+          <div className="input_container">
+          <input
+            className="form_input"
+            placeholder="Email"
+            {...register("email", { required: true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ })}
+          />
+          {errors.email && errors.email.type === "required" && <p className="error_message">Email wajib diisi</p>}
+          {errors.email && errors.email.type === "pattern" && <p className="error_message">Email tidak valid</p>}
+          </div>
+
+          <div className="input_container">
+          <input
+            className="form_input"
+            placeholder="Password"
+            {...register("password", { required: true })}
+          />
+          {errors.password && (
+            <p className="error_message">Password wajib diisi</p>
+          )}
+          </div>
+          
+
+          <input 
+          id="login_button" 
+          type="submit"
+          value='LOG IN'
+           />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- htt-frontend-test/src/router.jsx
+++ htt-frontend-test/src/router.jsx
@@ -1,34 +1,36 @@
-import { createBrowserRouter, redirect, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
 import App from "./App";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 const router = createBrowserRouter([
   {
     element: <App />,
     loader: () => {
-      let token = localStorage.getItem("access_token")
-      if(!token) {
-        return redirect('/login')
+      let token = localStorage.getItem("access_token");
+      if (!token) {
+        return redirect("/login");
       }
       return null;
     },
     children: [
       {
         path: "/",
-        element: <div>halaman utama!</div>,
+        element: <HomePage />,
       },
     ],
   },
-    {
-      path: "/login",
+  {
+    path: "/login",
     loader: () => {
-      let token = localStorage.getItem("access_token")
-      if(token) {
-        return redirect('/')
+      let token = localStorage.getItem("access_token");
+      if (token) {
+        return redirect("/");
       }
       return null;
     },
-    element: <div>halaman login</div>,
-    },
-  ]);
+    element: <LoginPage />,
+  },
+]);
 
-  export default router;
+export default router;
